19ec1352b4d555c862de4867096f345e0f8d5f81
Samuel Ajala <samuelajala01@gmail.com>
2022-01-13 13:47:35 +0100

    added menu-bar remove action

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ let year = new Date().getFullYear();
 document.querySelector('#footer-year').innerText = year;
 
 let menubar = document.querySelector('.menu-bar');
+
+menubar.onclick = () => {
+  menubar.remove();
+}
