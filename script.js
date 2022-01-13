a5aa7cf711c388ec9996c16f19007114861c4095
Samuel Ajala <samuelajala01@gmail.com>
2022-01-13 14:23:48 +0100

    added menu toggle  functionality

--- script.js
+++ script.js
@@ -4,7 +4,24 @@ let year = new Date().getFullYear();
 document.querySelector('#footer-year').innerText = year;
 
 let menubar = document.querySelector('.menu-bar');
+let menuList = document.querySelector('.menu-list');
+let menuRemoveBar = document.querySelector('.menu-remove-bar');
+
+function showMenu(){
+  menubar.style.display = 'none';
+  menuList.style.display = 'block';
+}
+
+function closeMenu(){
+  menuRemoveBar.style.display = 'none';
+  menubar.style.display = 'block';
+  menuList.remove();
+}
 
 menubar.onclick = () => {
-  menubar.remove();
+  showMenu();
+}
+
+menuRemoveBar.onclick = () => {
+  closeMenu();
 }
